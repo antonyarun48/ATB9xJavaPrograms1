ac202ea303b22d953b80b2491e60e441f9a3d44b
antonyarun48 <antonyarun48@gmail.com>
2024-10-06 16:48:00 +0530

    All the files added

--- src/Oct/ex_02102024_MainMethod/Lab002.java
+++ src/Oct/ex_02102024_MainMethod/Lab002.java
@@ -1,4 +1,12 @@
 package Oct.ex_02102024_MainMethod;
 
 public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(10+10);
+        System.out.println(10-2);
+        System.out.println(10*3);
+        System.out.println(10/5);
+
+
+    }
 }
